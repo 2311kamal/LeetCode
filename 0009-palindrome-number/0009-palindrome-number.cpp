e80cd967b23b74e6936a8a5d5e2f5d81e7b3ad60
2311kamal <96480223+2311kamal@users.noreply.github.com>
2023-06-02 15:45:20 +0530

    Time: 24 ms (22.38%), Space: 5.9 MB (50.84%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,9 +1,28 @@
-class Solution {
+// class Solution {
+// public:
+//     bool isPalindrome(int x) {
+//         string s = to_string(x);
+//         string t = s;
+//         reverse(t.begin(), t.end());
+//         return s == t;
+//     }
+// };
+
+
+
+class Solution
+{
 public:
-    bool isPalindrome(int x) {
-        string s = to_string(x);
-        string t = s;
-        reverse(t.begin(), t.end());
-        return s == t;
+    bool isPalindrome(int x)
+    {
+        if (x < 0 || (x != 0 && x % 10 == 0))
+            return false;
+        int sum = 0;
+        while (x > sum)
+        {
+            sum = sum * 10 + x % 10;
+            x = x / 10;
+        }
+        return (x == sum) || (x == sum / 10);
     }
 };
