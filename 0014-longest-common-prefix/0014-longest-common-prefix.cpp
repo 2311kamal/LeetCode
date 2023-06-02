d5944ca514b02dbe4e9239e9e2e0da9baeebf29e
2311kamal <96480223+2311kamal@users.noreply.github.com>
2023-06-02 17:46:55 +0530

    Time: 6 ms (38.39%), Space: 9.2 MB (72.51%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,27 +1,51 @@
+// class Solution
+// {
+// public:
+//     string longestCommonPrefix(vector<string> &str)
+//     {
+
+//         string ans = "";
+//         for (int i = 0; i < str[0].size(); i++)
+//         {
+
+//             char a = str[0][i];
+//             int j;
+//             for (j = 0; j < str.size(); j++)
+//             {
+
+//                 if (i >= str[j].size() || str[j][i] != a)
+//                     break;
+//             }
+//             if (j == str.size())
+//                 ans += a;
+//             else
+//             {
+//                  break;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+
 class Solution
 {
 public:
-    string longestCommonPrefix(vector<string> &str)
+    string longestCommonPrefix(vector<string> &v)
     {
-
         string ans = "";
-        for (int i = 0; i < str[0].size(); i++)
+        sort(v.begin(), v.end());
+        int n = v.size();
+        string first = v[0], last = v[n - 1];
+        for (int i = 0; i < min(first.size(), last.size()); i++)
         {
-
-            char a = str[0][i];
-            int j;
-            for (j = 0; j < str.size(); j++)
+            if (first[i] != last[i])
             {
-
-                if (i >= str[j].size() || str[j][i] != a)
-                    break;
+                return ans;
             }
-            if (j == str.size())
-                ans += a;
-            else
-            {
-                 break;
-            }
+            ans += first[i];
         }
         return ans;
     }
