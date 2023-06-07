caca3839db50dfeaebb81f3c8450d261ac65bcb5
2311kamal <96480223+2311kamal@users.noreply.github.com>
2023-06-08 04:21:40 +0530

    Time: 15 ms (74.06%), Space: 8.5 MB (54.91%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,14 +1,36 @@
-class Solution {
+
+
+class Solution
+{
 public:
-   int lengthOfLongestSubstring(string s) {
-        vector<int> dict(256, -1);
-        int maxLen = 0, start = -1;
-        for (int i = 0; i != s.length(); i++) {
-            if (dict[s[i]] > start)
-                start = dict[s[i]];
-            dict[s[i]] = i;
-            maxLen = max(maxLen, i - start);
+    int lengthOfLongestSubstring(string s)
+    {
+
+        unordered_map<char, int> m;
+        int len = 0,n=s.length();
+        for (int r = 0, l = 0; r < n; r++)
+        {
+
+            if (m.find(s[r]) != m.end())
+                l=max( m[s[r]] + 1, l);
+            m[s[r]] = r;
+            len = max(len, r - l + 1);
         }
-        return maxLen;
+        return len;
     }
 };
+
+// class Solution {
+// public:
+//    int lengthOfLongestSubstring(string s) {
+//         vector<int> dict(256, -1);
+//         int maxLen = 0, start = -1;
+//         for (int i = 0; i != s.length(); i++) {
+//             if (dict[s[i]] > start)
+//                 start = dict[s[i]];
+//             dict[s[i]] = i;
+//             maxLen = max(maxLen, i - start);
+//         }
+//         return maxLen;
+//     }
+// };
